133e48ff261470f5636907e564376e519a7926df
lauraM84 <laura.mazza84@gmail.com>
2025-06-09 13:34:29 +0200

    Add project files.

---
+++ chirpAPI/Model/CinguettioContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace chirpAPI.Model;
+
+public partial class CinguettioContext : DbContext
+{
+    public CinguettioContext()
+    {
+    }
+
+    public CinguettioContext(DbContextOptions<CinguettioContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Chirp> Chirps { get; set; }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=cinguettio;Username=postgres;Password=superpippo;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Chirp>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("chirp_pk");
+
+            entity.ToTable("chirps");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreationTime)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnName("creation_time");
+            entity.Property(e => e.ExtUrl)
+                .HasMaxLength(2083)
+                .HasColumnName("ext_Url");
+            entity.Property(e => e.Lat).HasColumnName("lat");
+            entity.Property(e => e.Lng).HasColumnName("lng");
+            entity.Property(e => e.Text)
+                .HasMaxLength(140)
+                .HasColumnName("text");
+        });
+
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("comment_pk");
+
+            entity.ToTable("comments");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.ChirpId).HasColumnName("chirp_id");
+            entity.Property(e => e.CreationTime)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("creation_time");
+            entity.Property(e => e.ParentId).HasColumnName("parent_id");
+            entity.Property(e => e.Text)
+                .HasMaxLength(140)
+                .HasColumnName("text");
+
+            entity.HasOne(d => d.Chirp).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.ChirpId)
+                .HasConstraintName("chirp_fk");
+
+            entity.HasOne(d => d.Parent).WithMany(p => p.InverseParent)
+                .HasForeignKey(d => d.ParentId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("parent_fk");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ chirpAPI/Model/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chirpAPI.Model;
+
+public partial class Comment
+{
+    public int Id { get; set; }
+
+    public int ChirpId { get; set; }
+
+    public int? ParentId { get; set; }
+
+    public string Text { get; set; } = null!;
+
+    public DateTime CreationTime { get; set; }
+
+    public virtual Chirp Chirp { get; set; } = null!;
+
+    public virtual ICollection<Comment> InverseParent { get; set; } = new List<Comment>();
+
+    public virtual Comment? Parent { get; set; }
+}
